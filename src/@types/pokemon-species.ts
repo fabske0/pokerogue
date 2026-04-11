467e935161fa5369d2b6b8f929889ea8f2974e6f
Fabi <192151969+fabske0@users.noreply.github.com>
2026-04-11 19:28:40 +0200

    add starters to gen 1

--- src/@types/pokemon-species.ts
+++ src/@types/pokemon-species.ts
@@ -3,6 +3,7 @@ import type { PokemonSpecies } from "#data/pokemon-species";
 import type { AbilityId } from "#enums/ability-id";
 import type { EggTier } from "#enums/egg-type";
 import type { MoveId } from "#enums/move-id";
+import type { SpeciesId } from "#enums/species-id";
 
 interface PokemonSpeciesPassives {
   [key: number]: AbilityId;
@@ -17,7 +18,9 @@ export interface SpeciesFormTmMoves {
 
 export interface PokemonSpeciesData {
   species: PokemonSpecies;
+  starter: SpeciesId;
   evolutions: SpeciesFormEvolution | SpeciesFormEvolution[];
+  /** The egg tier of the Pokémon. Should be omitted for non starters */
   eggTier?: EggTier;
   passives: AbilityId | PokemonSpeciesPassives;
   levelMoves: LevelMoves;
