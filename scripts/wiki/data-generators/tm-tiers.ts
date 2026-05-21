328104709a7232d6b40c6c9e0ab962c341b91496
Fabi <192151969+fabske0@users.noreply.github.com>
2026-05-21 19:01:07 +0200

    add `tm-tiers` script

---
+++ scripts/wiki/data-generators/tm-tiers.ts
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Pagefault Games
+ * SPDX-FileContributor: Fabske0
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { tmPoolTiers } from "#balance/tm-pool-tiers";
+import { ModifierTier } from "#enums/modifier-tier";
+import { MoveId } from "#enums/move-id";
+import { writeWikiData } from "../helpers";
+import type { TmTierWikiEntry } from "../types";
+
+export function generateTmTiersData(): void {
+  const entries: TmTierWikiEntry[] = [];
+
+  for (const [move, tier] of Object.entries(tmPoolTiers)) {
+    const data: TmTierWikiEntry = {
+      move: MoveId[move],
+      tier: ModifierTier[tier],
+    };
+    entries.push(data);
+  }
+
+  writeWikiData("tm-tiers", entries);
+}
