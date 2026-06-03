f0432490f4bc954ad380a8a403c7934ac5c9509a
Fabi <192151969+fabske0@users.noreply.github.com>
2026-06-03 22:21:12 +0200

    temp fix for crash

--- src/global-audio-manager.ts
+++ src/global-audio-manager.ts
@@ -1,3 +1,9 @@
-import { AudioManager } from "#audio/audio-manager";
+import type { AudioManager as GlobalAudioManager } from "#audio/audio-manager";
 
-export const audioManager = new AudioManager();
+export let audioManager: GlobalAudioManager;
+
+export async function initGlobalAudioManager() {
+  // Doing it this way to avoid a crash when running the wiki scraper script due to phaser-rex2 being imported
+  const { AudioManager } = await import("#audio/audio-manager");
+  audioManager = new AudioManager();
+}
